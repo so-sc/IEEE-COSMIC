6435def9f5f0c462e83e06a2d5799639de477686
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2024-01-29 16:25:01 +0530

    UI changes were made

---
+++ src/components/Committee.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const Committee = () => {
+  return (
+    <div>
+    <h1 className="font-bold text-2xl my-3">Conference Committee</h1>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Chief Patrons</p>
+      <p>Most Rev. Dr. Peter Paul Saldanha, Bishop, Diocese of Mangalore, India</p>
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Patrons</p>
+      <p>Rev. Fr Wilfred Prakash D'Souza, Director, SJEC Mangaluru, India</p>
+   <p>Fr Kenneth Rayner Crasta, Assistant Director, SJEC Mangaluru, India</p>
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Advisory Committee</p>
+      <p></p>Ashwin T S, Vanderbilt University,Tennessee, USA
+<p>B H Shekar, Mangalore University, India</p>
+<p>B. E. Rangaswamy, Visvesvaraya Technological University, Belagavi, India</p>
+<p>D. S. Guru, University of Mysore, India</p>
+<p>K. Chandrasekaran, NITK Surathkal, India</p>
+<p>Mohammed Javed, IIIT Allahabad, India</p>
+<p>Nagabhushan P., Vignan University, Andhra Pradesh, India</p>
+<p>Purushothama Chippar, SJEC Mangaluru, India</p>
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold' >General Chairs</p>
+      <p>Rio D'Souza, SJEC Mangaluru, India</p>
+      <p>Vasudeva, NMAMIT Nitte, India</p>
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Steering Committee</p>
+           <p>Aloknath De, Samsung, India</p>
+     <p>Annappa B., NITK Surathkal, India</p>
+     <p>Bindumadhava, C-DAC Bengaluru, India</p>
+     <p>Divya M. G., C-DAC Bengaluru, India</p>
+     <p>Manohar Pai M. M., MIT Manipal, India</p>
+     <p>Mohit P. Tahiliani, NITK Surathkal, India</p>
+     <p>Niranjan N. Chiplunkar, NMAMIT Nitte, India</p>
+     <p>Niranjan U. C., Chair (2020) IEEE Mangalore Subsection, India</p>
+     <p>Poornalatha G., MIT Manipal, India</p>
+     <p>Puneet Misra, ISRO Bengaluru, India</p>
+     <p>Pushparaj Shetty D., NITK Surathkal, India</p>
+          <p>T. Srinivasa, IISc Bangalore, India</p> 
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Program Chairs</p>
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Organizing Chairs</p>
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Publication Chairs</p>
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Finance Chairs</p>
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Sponsorship Chairs</p>
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Publication Chairs</p>
+    </div>
+    <div>
+      <p className='text-sm text-[#018100] font-bold'>Registration Chairs</p>
+    </div>
+  </div>
+  )
+}
+
+export default Committee
---
+++ src/components/Dates.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Dates = () => {
+  return (
+    <div className='space-y-4'>
+  <h1 className='text-2xl font-bold'>Important Dates</h1>
+<p> <span  className='font-semibold' > Paper Submission Deadline:</span> Friday, May 31, 2024 </p> 
+ <p> <span className='font-semibold'>Acceptance Notification:</span> Thursday August 8, 2024</p> 
+<p><span className='font-semibold'> Registration Deadline:</span> Wednesday, September 18, 2024</p> 
+<p><span className='font-semibold'> CRC Submission Deadline:</span> Wednesday, September 18, 2024</p> 
+<p><span className='font-semibold'> Conference Dates:</span> October 18 - 19, 2024 (Friday and Saturday)</p> 
+    </div>
+  )
+}
+
+export default Dates
