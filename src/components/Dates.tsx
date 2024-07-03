91fba89451cdfd833c1a97d458afc683b971aa57
srajan <kumarsrajan02@gmail.com>
2024-07-03 18:23:54 +0530

    update: dates

--- src/components/Dates.tsx
+++ src/components/Dates.tsx
@@ -5,20 +5,20 @@ const Dates = () => {
     <div className="space-y-4 my-3 text-sm">
       <h1 className="text-2xl font-bold">Important Dates</h1>
       <p>
-        <span className="font-semibold">Paper Submission Deadline:</span>{" "}
-        Friday, June 28, 2024
+        <span className="font-semibold">Paper Submission Deadline:</span> August
+        30, 2024
       </p>
       <p>
         <span className="font-semibold">Acceptance Notification:</span> Friday,
-        September 30, 2024
+        October 10, 2024
       </p>
       <p>
         <span className="font-semibold">Registration Deadline:</span> Friday,
-        October 18, 2024
+        October 25, 2024
       </p>
       <p>
         <span className="font-semibold">CRC Submission Deadline:</span> Friday,
-        October 18, 2024
+        October 25, 2024
       </p>
       <p>
         <span className="font-semibold">Conference Dates:</span> November 22 -
